88dfff3a8e13167376823c947053d0d89bc19451
Kulwinder <Kulwinder.singh@softobiz.com>
2023-04-11 12:29:20 +0530

    Implemented QuoteBlock component in the react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import QuoteBlock from "./components/QuoteBlock";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, setTheme] = useState("primary");
+
+	return (
+		<div className="App">
+			<header className="header">
+				<button
+					className="theme-swithcer"
+					onClick={() =>
+						setTheme(theme === "primary" ? "secondary" : "primary")
+					}
+				>
+					Switch theme
+				</button>
+			</header>
+			<QuoteBlock
+				title="Possit etimam"
+				subTitle="Morbi eget efficitur nunc."
+				hideUnderline={false}
+				description="A staycation with the kids is always a good idea especially at Dubai’s family resort! Memorable bonding time with complimentary lunch or dinner at Gordon Ramsay’s Bread Street Kitchen*, activities for kids, one hour free watersports in the Arabian Sea and so much more! We’re sure to keep the little ones entertained."
+				hyperlinkText="Test Hyperlink"
+				hyperlinkLink="https://www.google.com"
+				ctaPrimaryText="Buy ticket"
+				ctaPrimaryLink="https://reactjs.org/"
+				ctaSecondaryText="Buy ticket"
+				ctaSecondaryLink="https://reactjs.org/"
+				compBackground="white"
+				textboxBackground="cream"
+				textBoxTextColor="black"
+				theme={theme}
+			/>
+		</div>
+	);
 }
 
 export default App;
